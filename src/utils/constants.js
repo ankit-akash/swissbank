42251faa01b2ec962c1ef46fd9bc6f1a3d86690a
Ankit Akash <75488501+ankit-akash@users.noreply.github.com>
2023-10-30 22:36:52 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -37,7 +37,7 @@ export const categories = [
 export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
 export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
 export const demoVideoUrl = "/video/GDa8kZLNhJ4";
-export const demoChannelTitle = "riturani";
+export const demoChannelTitle = "ankitakash";
 export const demoVideoTitle = "RapidAPI";
 export const demoProfilePicture =
   "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
